5aea77cda2548d1da43482d316fb48ed50d6b4bf
Kristina Guseynova <bittersweet2010@list.ru>
2024-09-17 22:37:45 +0400

    added test file with 'open page' action

---
+++ src/test/java/tests/SelenideSearchTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideSearchTest {
+
+    @Test
+    void shouldHaveSoftAssertions() {
+
+        //открыть страницу
+        open("https://github.com");
+        //ввести в инпут поисковый запрос
+
+    }
+
+}
